7e80daf3bafe98de1f6230986b8b857349d260c8
karnbhushan1994 <karn.becs.13@gmail.com>
2024-04-06 23:21:52 +0530

    env file change

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
-// server.js
+// Load environment variables from config.env using dotenv
+require("dotenv").config({ path: "./config/config.env" });
 
-const app = require("./app"); // Corrected 'required' to 'require'
+// Importing the Express application from the app.js file
+const app = require("./app");
 
+// Setting up the port to listen on, defaulting to 3000 if not provided
 const PORT = process.env.PORT || 3000;
 
+// Starting the server and listening on the specified port
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
